606d0ceb65d654d3733a51b39c990e90468cc129
Amange1di <amangeldi2702006@gmail.com>
2026-05-10 15:26:09 +0600

    эр

--- backend/edudata/urls.py
+++ backend/edudata/urls.py
@@ -3,8 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
+    path("health/", health_check),
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
     path("api/assets/", include("assets.urls")),
